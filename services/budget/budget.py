acedcaaf4842165a71742ed7b55a7b9bbcc731c5
isaieko <vlad.isaenko@icloud.com>
2026-02-16 19:49:12 +0100

    deposit method + tests

--- services/budget/budget.py
+++ services/budget/budget.py
@@ -1,5 +1,15 @@
+from __future__ import annotations
+
 class Category:
-    def __init__(self, name):
-        self.name = name
-        self.ledger = []
+    def __init__(self, name: str):
+        self.name: str = name
+        self.ledger: list[dict[str, object]] = []
+
+    def deposit(self, amount: int | float, description: str = "") -> None:
+        if not isinstance(amount, (int, float)):
+            msg = f"amount must be int or float and not {type(amount)}"
+            raise TypeError(msg)
+        if amount < 0:
+            raise ValueError("amount must be non-negative")
+        self.ledger.append({"amount": amount, "description": description})
 
--- services/budget/budget_test.py
+++ services/budget/budget_test.py
@@ -1,12 +1,29 @@
 from budget import Category
 import pytest
 
-def test_init_category():
-    food = Category("Food")
-    personal = Category("Personal")
 
+food = Category("Food")
+personal = Category("Personal")
+
+def test_init_category():
     assert food.name == "Food"
     assert personal.name == "Personal"
     assert len(food.ledger) == 0
     assert len(personal.ledger) == 0
+
+def test_deposit():
+    with pytest.raises(ValueError):
+        food.deposit(-100, "negative amount")
+    with pytest.raises(TypeError):
+        personal.deposit("100")
+
+    food.deposit(100, "Initial deposit")
+    personal.deposit(1000)
+
+    assert len(food.ledger) > 0
+    assert len(personal.ledger) > 0
+    assert food.ledger[0]["amount"] == 100
+    assert food.ledger[0]["description"] == "Initial deposit"
+    assert personal.ledger[0]["amount"] == 1000
+    assert personal.ledger[0]["description"] == ""
 
