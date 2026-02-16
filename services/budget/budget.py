aa794b45d6733be645110565a8bb083c3f55a696
isaieko <vlad.isaenko@icloud.com>
2026-02-16 20:56:10 +0100

    get_balance, check_funds, withdraw, transfer methods + tests

--- services/budget/budget.py
+++ services/budget/budget.py
@@ -6,10 +6,70 @@ class Category:
         self.ledger: list[dict[str, object]] = []
 
     def deposit(self, amount: int | float, description: str = "") -> None:
+        """
+        Add a deposit to the ledger.
+        """
         if not isinstance(amount, (int, float)):
             msg = f"amount must be int or float and not {type(amount)}"
             raise TypeError(msg)
         if amount < 0:
             raise ValueError("amount must be non-negative")
         self.ledger.append({"amount": amount, "description": description})
+
+    def get_balance(self) -> float:
+        """
+        Calculate and return the current balance.
+        """
+        return sum(item["amount"] for item in self.ledger)
+
+    def check_funds(self, amount: int | float) -> bool:
+        """
+        Check if enough funds exist for the given amount.
+        Return True if sufficient, otherwise False.
+        """
+        if not isinstance(amount, (int, float)):
+            msg = f"amount must be int or float and not {type(amount)}"
+            raise TypeError(msg)
+        if amount < 0:
+            raise ValueError("amount must be non-negative")
+        return amount <= self.get_balance()
+
+    def withdraw(self, amount: int | float, description: str = "") -> bool:
+        """
+        Add a withdrawal (as a negative amount) if funds are sufficient.
+        Return True if successful, otherwise False.
+        """
+        if not isinstance(amount, (int, float)):
+            msg = f"amount must be int or float and not {type(amount)}"
+            raise TypeError(msg)
+        if amount < 0:
+            raise ValueError("amount must be non-negative")
+        if not self.check_funds(amount):
+            return False
+        else:
+            self.ledger.append({"amount": -amount, "description": description})
+            return True
+
+
+    def transfer(self, amount: int | float, other_category: "Category") -> bool:
+        """
+        Transfer amount to another Category.
+        Record a withdrawal in this category and
+        a deposit in the other category if funds are sufficient.
+        Return True if successful, otherwise False.
+        """
+        if not isinstance(amount, (int, float)):
+            msg = f"amount must be int or float and not {type(amount)}"
+            raise TypeError(msg)
+        if amount < 0:
+            raise ValueError("amount must be non-negative")
+        if not isinstance(other_category, Category):
+            msg = f"amount must be Category and not {type(other_category)}"
+            raise TypeError(msg)
+        if not self.check_funds(amount):
+            return False
+
+        self.withdraw(amount, f"Transfer to {other_category.name}")
+        other_category.deposit(amount, f"Transfer from {self.name}")
+        return True
 
--- services/budget/budget_test.py
+++ services/budget/budget_test.py
@@ -27,3 +27,41 @@ def test_deposit():
     assert personal.ledger[0]["amount"] == 1000
     assert personal.ledger[0]["description"] == ""
 
+def test_check_funds():
+    with pytest.raises(ValueError):
+        food.check_funds(-100)
+    with pytest.raises(TypeError):
+        personal.check_funds("100")
+
+    assert food.check_funds(100) == True
+    assert food.check_funds(1000) == False
+
+    assert personal.check_funds(100) == True
+    assert personal.check_funds(1001) == False
+
+def test_withdraw():
+    test = Category("Test")
+    with pytest.raises(ValueError):
+        test.withdraw(-100)
+    with pytest.raises(TypeError):
+        test.withdraw("100")
+
+    assert test.withdraw(0, "withdraw zero") == True
+    test.deposit(100, "test deposit")
+    assert test.withdraw(100, "test_withdraw") == True
+    assert test.withdraw(10) == False
+
+def test_transfer():
+    a = Category("A")
+    b = Category("B")
+
+    with pytest.raises(TypeError):
+        a.transfer("10", b)
+        a.transfer(10, 2)
+
+    with pytest.raises(ValueError):
+        a.transfer(-10, b)
+
+    a.deposit(10, "test deposit")
+    assert a.transfer(10, b) == True and b.get_balance() == 10
+    assert b.transfer(100, a) == False and b.get_balance() == 10
