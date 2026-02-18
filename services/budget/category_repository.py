27147a2c62721b0487172b9ad1e46e925d109d2d
isaieko <vlad.isaenko@icloud.com>
2026-02-18 23:04:28 +0100

    adding object style exceptions to the category_repository.py

--- services/budget/category_repository.py
+++ services/budget/category_repository.py
@@ -1,6 +1,50 @@
 from __future__ import annotations
-from typing import Protocol
+from typing import Iterable, Protocol
+from dataclasses import dataclass
+from enum import Enum
 from budget import Category
 
+class CategoryCreateError(Enum):
+    ALREADY_EXISTS = "already_exists"
+    UNKNOWN = "unknown"
+
+class CategoryGetError(Enum):
+    DONT_EXIST = "Category doesn't exist"
+    UNKNOWN = "unknown"
+class CategoryUpdateError(Enum):
+    DONT_EXIST = "Category doesn't exist"
+    UPDATE_ERROR = "Error while updateing a record"
+    UNKNOWN = "unknown"
+
+
+@dataclass
+class CategoryCreateResult:
+    category: Category | None
+    error: CategoryCreateError | None
+@dataclass
+class CategoryGetResult:
+    category: Category | None
+    error: CategoryGetError | None
+@dataclass
+class CategoryUpdateResult:
+    category: Category | None
+    error: CategoryUpdateError | None
+
+
+
 class CategoryRepository(Protocol):
-    def create_category(self, user_id: str, category: Category) -> Category: ...
+    def create_category(self, user_id: str, category: Category) -> CategoryCreateResult:
+        ...
+    def get_category(self, user_id: str, category_id: str) -> CategoryGetError:
+        ...
+    def list_categories(self, user_id: str) -> Iterable[Category]:
+        ...
+    def update_category(self, user_id: str, category_id: str, **kwargs) -> CategoryUpdateError:
+        ...
+    def delete_category(self, user_id: str, category_id: str) -> bool:
+        ...
+    def get_category_balance(self, user_id: str, category_id: str) -> float:
+        ...
+
+
+
